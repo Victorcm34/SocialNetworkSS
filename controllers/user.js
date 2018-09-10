1360a38ab8170715124d696be2b01173afeddfc9
victorcm34 <victorcm34@gmail.com>
2018-09-10 19:04:02 +0200

    uploadImage working

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,8 @@ var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
 var jwt = require('../services/jwt');
 var mongoosePaginate = require('mongoose-pagination');
+var fs = require('fs');
+var path = require('path');
 
 //Metodos de prueba
 function home(req, res) {
@@ -137,11 +139,71 @@ function getUsers(req, res) {
     });
 }
 
+//Editar datos usuarios
+function updateUser(req, res) {
+    var userId = req.params.id;
+    var update = req.body;
+
+    //borrar propiedad password
+    delete update.password;
+
+    if(userId != req.user.sub){
+        return res.status(500).send({message: 'No tienes permiso para actualizar los datos de este usuario'});
+    }
+
+    User.findByIdAndUpdate(userId, update, {new: true}, (err, userUpdated) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+        return res.status(200).send({user: userUpdated});
+    });
+}
+
+//Subir archivos de imagen/avatar de usuario
+function uploadImage(req, res) {
+    var userId = req.params.id;
+
+    if(req.files) {
+        var file_path = req.files.image.path;
+        console.log(file_path);
+        var file_split = file_path.split('\\');
+        console.log(file_split);
+        var file_name = file_split[2];
+        console.log(file_name);
+        var ext_split = file_name.split('\.');
+        console.log(ext_split);
+        var file_ext = ext_split[1];
+        console.log(file_ext);
+
+        if(userId != req.user.sub){
+            removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar los datos de este usuario');            
+        }
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+            //Actualizar imagen de usuario logueado
+        }else{
+            removeFilesOfUploads(res, file_path, 'Extensión no válida');
+        }
+
+    }else {
+        return res.status(200).send({message: 'No se han subido archivos de imagen'});
+    }
+}
+
+function removeFilesOfUploads(res, file_path, message){
+    fs.unlink(file_path, (err) => {
+        return res.status(200).send({message: message});
+    });
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
     loginUser,
     getUser,
-    getUsers
+    getUsers,
+    updateUser,
+    uploadImage
 }
