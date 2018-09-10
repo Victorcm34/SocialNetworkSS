fa1191fae270cc9d9065f3f61445015ef2e95f76
victorcm34 <victorcm34@gmail.com>
2018-09-10 12:11:51 +0200

    getUser created

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
 var jwt = require('../services/jwt');
 
+//Metodos de prueba
 function home(req, res) {
     res.status(200).send({
         message: 'Hola Mundo desde el servidor de NodeJs'
@@ -16,6 +17,7 @@ function pruebas(req, res) {
     });
 };
 
+//Registro de usuarios
 function saveUser(req, res){
     var params = req.body;
     var user = new User();
@@ -64,6 +66,7 @@ function saveUser(req, res){
     }
 }
 
+//Login de usuarios
 function loginUser(req,res){
     var params = req.body;
 
@@ -96,9 +99,23 @@ function loginUser(req,res){
     });
 }
 
+//Conseguir datos de un usuario
+function getUser(req, res) {
+    var userId = req.params.id;
+
+    User.findById(userId, (err, user) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(!user) return res.status(404).send({message: 'El usuario no existe'});
+
+        return res.status(200).send({user});
+    });
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    getUser
 }
