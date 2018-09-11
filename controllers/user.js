bb0f9d48361f522f565558a13fc64896e5b88c8f
victorcm34 <victorcm34@gmail.com>
2018-09-11 12:28:42 +0200

    follow functions working

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var User = require('../models/user');
+var Follow = require('../models/follow');
 var bcrypt = require('bcrypt-nodejs');
 var jwt = require('../services/jwt');
 var mongoosePaginate = require('mongoose-pagination');
@@ -111,8 +112,28 @@ function getUser(req, res) {
 
         if(!user) return res.status(404).send({message: 'El usuario no existe'});
 
-        return res.status(200).send({user});
+        followThisUser(req.user.sub, userId).then((value) => {
+            user.password = undefined;
+            return res.status(200).send({user, 
+                                        following: value.following,
+                                        followed: value.followed });
+        }); 
     });
+}
+
+async function followThisUser(identity_user_id, user_id) {
+    var following = await Follow.findOne({"user": identity_user_id, "followed": user_id}).exec((err, follow) => {    
+        if(err) return handleError(err);
+        return follow;
+    });
+    var followed = await Follow.findOne({"user": user_id, "followed": identity_user_id}).exec((err, follow) => {    
+        if(err) return handleError(err);
+        return follow;
+    });
+    return {
+        following: following,
+        followed: followed
+    }
 }
 
 //Devolver un listado de usuarios paginado
@@ -204,6 +225,7 @@ function removeFilesOfUploads(res, file_path, message){
     });
 }
 
+// Obtener imagen de usuario
 function getImageFile(req, res) {
     var image_file = req.params.imageFile;
     var path_file = './uploads/users/'+image_file;
